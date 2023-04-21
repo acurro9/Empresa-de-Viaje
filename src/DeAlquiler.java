9278c0db125c24f86e3a85f1af254a5c83ad81f8
acurro9 <asbarxas.aaroncurro@gmail.com>
2023-04-21 15:02:45 +0100

    Se añaden los setters y los getters

--- src/DeAlquiler.java
+++ src/DeAlquiler.java
@@ -8,6 +8,7 @@ public class DeAlquiler {
     int codBarco;
     String nombreBarco;
 
+    //Constructores
     DeAlquiler (String nombre, int CIF, String domFiscal, int precio,
                 int barcos, int maxTripulacion, int codBarco, String nombreBarco){
         this.nombre=nombre;
@@ -18,5 +19,58 @@ public class DeAlquiler {
         this.maxTripulacion=maxTripulacion;
         this.codBarco=codBarco;
         this.nombreBarco=nombreBarco;
+    }
+    DeAlquiler(){};
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCIF(int CIF) {
+        this.CIF = CIF;
+    }
+    public void setDomFiscal(String domFiscal) {
+        this.domFiscal = domFiscal;
+    }
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    public void setBarcos(int barcos) {
+        this.barcos = barcos;
+    }
+    public void setMaxTripulacion(int maxTripulacion) {
+        this.maxTripulacion = maxTripulacion;
+    }
+    public void setCodBarco(int codBarco) {
+        this.codBarco = codBarco;
+    }
+    public void setNombreBarco(String nombreBarco) {
+        this.nombreBarco = nombreBarco;
+    }
+
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCIF() {
+        return CIF;
+    }
+    public String getDomFiscal() {
+        return domFiscal;
+    }
+    public int getPrecio() {
+        return precio;
+    }
+    public int getBarcos() {
+        return barcos;
+    }
+    public int getMaxTripulacion() {
+        return maxTripulacion;
+    }
+    public int getCodBarco() {
+        return codBarco;
+    }
+    public String getNombreBarco() {
+        return nombreBarco;
     }
 }
